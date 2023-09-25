5817e965a9c40a5cfb0aa4b9ca94d5597581ab1c
nisanigarbal <143124801+nisanigarbal@users.noreply.github.com>
2023-09-25 21:10:30 +0300

    Add files via upload

---
+++ kdv_hesaplama.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class kdv_hesaplama {
+    public static void main ( String[]args){
+        double tutar, kdvOran= 0.18, kdvTutar ,kdvliTutar;
+        Scanner input = new Scanner(System.in);
+        System.out.print ("Ücret tutarı giriniz: ");
+        tutar = input.nextDouble();
+
+        kdvTutar = tutar * kdvOran;
+        kdvliTutar = tutar + kdvTutar;
+
+        System.out.println("kdvsiz tutar:" + tutar);
+        System.out.println("kdv oranı : " + kdvOran);
+        System.out.println("kdv tutarı: " + kdvTutar);
+        System.out.println("kdv'li tutar:" + kdvliTutar);
+
+    }
+}
